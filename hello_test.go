ee6dd5be7b54555693f0d4bbae0f83279c429028
Froos03 <Fariskhatib03@gmail.com>
2026-02-15 01:30:16 +0300

    refactored hello.go and added spanish, and french options

--- hello_test.go
+++ hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("sayin hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' to people when an empty string is supplied", func(t *testing.T) {
-		got:= Hello("")
-		want:= "Hello World"
+		got := Hello("", "English")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Marcel", "French")
+		want := "Bonjour, Marcel"
 
 		assertCorrectMessage(t, got, want)
 	})
