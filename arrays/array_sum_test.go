ca90f6d7022cddafd7b30de6c0749aba24bcd3bf
Froos03 <Fariskhatib03@gmail.com>
2026-03-04 03:47:02 +0300

    added sumall and sumalltail functions

--- arrays/array_sum_test.go
+++ arrays/array_sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -11,4 +14,41 @@ func TestSum(t *testing.T) {
 	if actual != expected {
 		t.Errorf("expected %d but actual is %d", expected, actual)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2, 3, 4, 5}, []int{0, 9})
+	expected := []int{15, 9}
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("expected %v but actual is %v", expected, actual)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, actual, expected []int) {
+		t.Helper()
+		if !slices.Equal(actual, expected) {
+			t.Errorf("got %v want %v", actual, expected)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		actual := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("safely sum slice of length 1", func(t *testing.T) {
+		actual := SumAllTails([]int{1}, []int{2})
+		expected := []int{0, 0}
+		checkSums(t, actual, expected)
+	})
 }
